6b7de18fb325f0986b4ab4035cbe4ef6761935ce
luistry <luisjaviermartinezlopez684[1;7rgmail.com>
2024-07-01 12:22:15 -0400

    fixed names

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,5 +1,5 @@
 import '@/app/globals.css';
-import SignUp from "@/app/components/routes-login-sign-up/Signup-";
+import SignUp from '@/app/components/routes-login-sign-up/Signup-';
 import Header from "@/app/components/Header";
 const SignUpage = () => {
     return (
